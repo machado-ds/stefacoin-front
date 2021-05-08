198ae07dc41c3d2257fb5f2c4cddc86e487d5da9
Davide Viana <davidjb.vr@gmail.com>
2021-05-08 20:20:04 -0300

    remocao rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './guards/auth-guard.service';
 import { HomeComponent } from './pages/private/home/home.component';
-import { ListarProfessorComponent } from './pages/private/professor/listar-professor/listar-professor.component';
 import { CadastroComponent } from './pages/public/cadastro/cadastro.component';
 import { LoginComponent } from './pages/public/login/login.component';
 import { PaginaNaoEncontradaComponent } from './pages/public/pagina-nao-encontrada/pagina-nao-encontrada.component';
@@ -12,12 +11,6 @@ const routes: Routes = [
     path: '',
     canActivate: [AuthGuardService],
     component: HomeComponent,
-    children: [
-      {
-        path: 'professor',
-        component: ListarProfessorComponent,
-      },
-    ],
   },
   {
     path: 'nova-conta',
