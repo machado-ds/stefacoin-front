9e3629614e7a7be205ebd779e14c81972de78a83
Davide Viana <davidjb.vr@gmail.com>
2021-05-08 14:52:52 -0300

    implementacao basica http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,6 +9,8 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AuthGuardService } from './guards/auth-guard.service';
+import { HttpInterceptorService } from './interceptors/http-interceptor.service';
 import { HomeComponent } from './pages/private/home/home.component';
 import { ListarProfessorComponent } from './pages/private/professor/listar-professor/listar-professor.component';
 import { CadastroComponent } from './pages/public/cadastro/cadastro.component';
@@ -44,7 +46,15 @@ export function tokenGetter() {
       },
     }),
   ],
-  providers: [],
+  providers: [
+    HttpInterceptorService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    },
+    AuthGuardService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
